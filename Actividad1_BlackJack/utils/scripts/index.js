ef8377cf0fbc3a28ab91daecd08ac83c0123a77c
Jose Aranguren <jmaranguren89@gmail.com>
2023-11-22 20:29:35 +0100

    crea y baraja las cartas

--- Actividad1_BlackJack/utils/scripts/index.js
+++ Actividad1_BlackJack/utils/scripts/index.js
@@ -1,8 +1,43 @@
 //Creamos la baraja
 
 let baraja = [];
+let palos = ['T','C','P','D']
+let boton = document.getElementById('btnstart');
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+
+
+boton.addEventListener( "click" , () => starGame())
+
+function starGame() {
+
+    //Funcion que inicia el juego
+
+    //Creamos baraja
+    for (let i = 0; i < palos.length; i++) {
+        for (let j = 1; j <= 13; j++) {
+            
+            baraja.push(`${j}${palos[i]}`)
+        }
+    }
+
+   barajada = mezclarBaraja(baraja)
+    
+    console.log(barajada)
     
 }
+
+
+
+
+
+function mezclarBaraja(baraja) {
+    for (let i = baraja.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [baraja[i], baraja[j]] = [baraja[j], baraja[i]]; // Intercambia elementos
+    }
+
+    return baraja
+}
+
+
+
