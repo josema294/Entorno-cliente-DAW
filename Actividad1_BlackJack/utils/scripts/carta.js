cb5a8ac99003a0dc37bf510d12c05a2c8e4a2e00
Jose Aranguren <jmaranguren89@gmail.com>
2023-11-26 18:25:02 +0100

    Pedir cartas funcional

--- Actividad1_BlackJack/utils/scripts/carta.js
+++ Actividad1_BlackJack/utils/scripts/carta.js
@@ -1,104 +1,110 @@
- class Carta {
+class Carta {
 
-    constructor(value){
+    constructor(infoCarta) {
 
-        this.value = value
-        this.palo = this.palo(value)
-        this.numero = this.numero(value)
-        this.img = this.setImg()
-        
-    }
+        this.infoCarta = infoCarta
+        this.determinaPalo(infoCarta)
+        this.palo
+        this.paloExtendido
 
-    valor(){
-        return this.value
-    }
+        this.numero(infoCarta)
+        this.value
+        this.carta
+        this.setImg()
+        this.imgSource
 
-  palo(value){
-    
-    if(value.includes('T')){
-        this.palo = "T"
-        this.paloExtendido = "Treboles"
-    }
-    else if (value.includes('C')) {
-        this.palo = "C"
-        this.paloExtendido = "Corazones"
-    }
-    else if (value.includes('P')) {
-        this.palo = "P"
-        this.paloExtendido = "Picas"
-    }
-    else {this.palo = "D"
-    this.paloExtendido = "Diamantes"}
-
-    
-  }
-
-  numero (value) {
-
-    switch (value) {
-        case value.includes('1'):
-            this.value = 1
-            this.carta=1
-            break;
-        case value.includes('2'):
-            this.value = 2
-            this.carta=2
-            break;
-        case value.includes('3'):
-            this.value = 3
-            this.carta=3
-            break;
-        case value.includes('4'):
-            this.value = 4
-            this.carta=4
-            break;
-        case value.includes('5'):
-            this.value = 5
-            this.carta=5
-            break;
-        case value.includes('6'):
-            this.value = 6
-            this.carta=6
-            break;
-        case value.includes('7'):
-            this.value = 7
-            this.carta=7
-            break;
-        case value.includes('8'):
-            this.value = 8
-            this.carta=8
-            break;
-        case value.includes('9'):
-            this.value = 9
-            this.carta=9
-            break;
-        case value.includes('10'):
-            this.value = 10
-            this.carta=10
-            break;
-        case value.includes('11'):
-            this.value = 11
-            this.carta='J'
-            break;
-        case value.includes('12'):
-            this.value = 11
-            this.carta='Q'
-            break;
-        case value.includes('13'):
-            this.value = 11
-            this.carta='K'
-            break;
-        default:
-            break;
     }
 
 
-  }
+    determinaPalo(value) {
 
-  setImg() {
+        if (value.includes('T')) {
+            this.palo = "T"
+            this.paloExtendido = "Treboles"
+        }
+        else if (value.includes('C')) {
+            this.palo = "C"
+            this.paloExtendido = "Corazones"
+        }
+        else if (value.includes('P')) {
+            this.palo = "P"
+            this.paloExtendido = "Picas"
+        }
+        else {
+            this.palo = "D"
+            this.paloExtendido = "Diamantes"
+        }
 
-    `../Actividad1_BlackJack/utils/images/${this.carta}${this.palo}`
-  }
+
+    }
+
+    numero(infoCarta) {
+        let valorNumerico = parseInt(infoCarta);
+
+        switch (valorNumerico) {
+            case valorNumerico = 1:
+                this.value = 1
+                this.carta = 1
+                break;
+            case valorNumerico = 2:
+                this.value = 2
+                this.carta = 2
+                break;
+            case valorNumerico = 3:
+                this.value = 3
+                this.carta = 3
+                break;
+            case valorNumerico = 4:
+                this.value = 4
+                this.carta = 4
+                break;
+            case valorNumerico = 5:
+                this.value = 5
+                this.carta = 5
+                break;
+            case valorNumerico = 6:
+                this.value = 6
+                this.carta = 6
+                break;
+            case valorNumerico = 7:
+                this.value = 7
+                this.carta = 7
+                break;
+            case valorNumerico = 8:
+                this.value = 8
+                this.carta = 8
+                break;
+            case valorNumerico = 9:
+                this.value = 9
+                this.carta = 9
+                break;
+            case valorNumerico = 10:
+                this.value = 10
+                this.carta = 10
+                break;
+            case valorNumerico = 11:
+                this.value = 11
+                this.carta = 'J'
+                break;
+            case valorNumerico = 12:
+                this.value = 11
+                this.carta = 'Q'
+                break;
+            case valorNumerico = 13:
+                this.value = 11
+                this.carta = 'K'
+                break;
+            default:
+                break;
+        }
+
+
+    }
+
+    setImg() {
+        this.imgSource = `./utils/images/${this.carta}${this.palo}.png`
+
+    }
 
 }
 
--- Actividad1_BlackJack/utils/scripts/index.js
+++ Actividad1_BlackJack/utils/scripts/index.js
@@ -1,41 +1,115 @@
 const baraja = []
 let jugador = ""
-let palos = ['T','C','P','D']
-let boton = document.getElementById('btnstart')
+let palos = ['T', 'C', 'P', 'D']
+const boton = document.getElementById('btnstart')
 let barajada = [];
-let marcadorBanca = 0
-let marcadorJugador = 0;
-const zonaBanca = document.getElementById('banca')
+let marcadorBanca = document.getElementById("marcadorBanca")
+let marcadorJugador = document.getElementById("marcadorJugador")
+let bancaPuntuacion = 0
+let jugadorPuntuacion = 0
+const zonaBanca = document.getElementById("banca")
+const zonajugador = document.getElementById("jugador")
+const interfaz = document.getElementById("interfaz")
 
-boton.addEventListener( "click" , () => starGame())
+
+boton.addEventListener("click", () => starGame())
+
 
 function starGame() {
-    
-    jugador = prompt("Por favor, introduce tu nombre:") 
+
+    jugador = prompt("Por favor, introduce tu nombre:")
     document.getElementById("nombrejugador").textContent = `Jugador: ${jugador}`
     creaBaraja()
     barajada = mezclarBaraja(baraja)
 
     //La banca empieza a jugar
 
-    while (marcadorBanca<=17) {
 
-    const carta = new Carta(barajada.pop())
-    marcadorBanca += carta.valor()
-    let imagen = document.createElement('img')
-    imagen.src= carta.img
-    zonaBanca.appendChild(imagen)
-        
+    while (bancaPuntuacion <= 17) {
+
+        let carta = new Carta(barajada.pop())
+        let imagen = document.createElement('img')
+        imagen.src = carta.imgSource
+        imagen.width = 150
+        imagen.height = 300
+        zonaBanca.appendChild(imagen)
+        bancaPuntuacion += carta.value
+        marcadorBanca.textContent = `Banca puntos: ${bancaPuntuacion}`
+
+        if (bancaPuntuacion > 21) {
+
+            BancaPierde()
+
+        }
+
     }
 
-     
+
+    //El jugador empieza
+
+    const botonPedir = document.createElement('button');
+    botonPedir.addEventListener("click", () => pideCarta())
+
+    botonPedir.textContent = "Pedir carta"
+    botonPedir.id = "botonPedir"
+    const botonPlantarse = document.createElement('button');
+    botonPlantarse.addEventListener("click", () => plantate())
+
+    botonPlantarse.textContent = "Plantarse"
+    botonPlantarse.id = "botonPlantarse"
+    interfaz.appendChild(botonPedir)
+    interfaz.appendChild(botonPlantarse)
+
+
+}
+
+function pideCarta() {
+
+    let carta = new Carta(barajada.pop())
+    let imagen = document.createElement('img')
+    imagen.src = carta.imgSource
+    imagen.width = 150
+    imagen.height = 300
+    zonajugador.appendChild(imagen)
+
+    jugadorPuntuacion += carta.value
+    marcadorJugador.textContent = `Tus puntos: ${jugadorPuntuacion}`
+
+
+    if (jugadorPuntuacion > 21) {
+
+        "has perdido, te has pasado de 21"
+
+    }
+
+
+}
+
+function BancaPierde() {
+
+    alert("La banca Ha perdido, ha excedido la puntuacion de 21")
+
+}
+
+function plantate() {
+
+    if (bancaPuntuacion > jugadorPuntuacion) {
+
+    }
+    else if (bancaPuntuacion = jugadorPuntuacion) {
+
+    }
+    else {
+
+    }
+
 }
 
 function creaBaraja() {
 
     for (let i = 0; i < palos.length; i++) {
         for (let j = 1; j <= 13; j++) {
-           
+
             baraja.push(`${j}${palos[i]}`)
         }
     }
