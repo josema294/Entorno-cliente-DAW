60d7cd70ddaad79f0edc422e79ecb5f15e6b5755
Jose Aranguren <jmaranguren89@gmail.com>
2023-12-12 19:36:11 +0100

    Mejorado ejercicio 5

--- NoEvaluables/Ejer5Opera/scriptOpera.js
+++ NoEvaluables/Ejer5Opera/scriptOpera.js
@@ -1,47 +1,48 @@
 let boton = document.querySelector("button")
 let num1
 let num2
-let sigue = true
-
-boton.addEventListener("click", (e) => {
-
-    while (sigue) {
-        num1 = parseInt(prompt("Introduce el primer numero con el que quieres operar"))
-        console.log(typeof (num1))
-        if (num1 < 0 || isNaN(num1)) {
-            alert("Solo son validos numeros y que sean mayores que 0")
-            sigue = false
-        }
-        if (sigue) {
-            num2 = parseInt(prompt("Introduce el segundo numero con el que quieres operar"))
-        }
-        if (num2 < 0 || isNaN(num2) && sigue) {
-            alert("Solo son validos numeros y que sean mayores que 0")
-            sigue = false
-        }
-
-        if (sigue) {
 
 
-            alert(`Los resultados son:
+function operaciones() {
+
+    let sigue = true
+
+    num1 = parseInt(prompt("Introduce el primer numero con el que quieres operar"))
+    console.log(typeof (num1))
+    if (num1 < 0 || isNaN(num1)) {
+        alert("Solo son validos numeros y que sean mayores que 0")
+        sigue = false
+    }
+    if (sigue) {
+        num2 = parseInt(prompt("Introduce el segundo numero con el que quieres operar"))
+    }
+    if (num2 < 0 || isNaN(num2) && sigue) {
+        alert("Solo son validos numeros y que sean mayores que 0")
+        sigue = false
+    }
+
+    if (sigue) {
+
+
+        alert(`Los resultados son:
          Suma: ${num1 + num2},
          Multiplicacion: ${num1 * num2}
          resta: ${num1 - num2}
          Division: ${num1 / num2}`)
 
-        }
-
-        if ((num1 - num2) < 0) {
-            sigue = false
-        }
-
-        if (sigue && prompt("Desea continuar con mas operaciones?? Responda si o no") === "si") {
-            sigue = true
-        }
-        else {
-            sigue = false
-        }
-
-
     }
-})
+
+    if ((num1 - num2) < 0) {
+        sigue = false
+    }
+
+    if (sigue && prompt("Desea continuar con mas operaciones?? Responda si o no") === "si") {
+
+        operaciones()
+    }
+    else {
+        sigue = false
+    }
+}
+
+boton.addEventListener("click",operaciones)
