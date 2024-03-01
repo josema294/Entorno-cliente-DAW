c5bcca405189fd23157c60dde92a7ebcbc21dda1
Jose Aranguren <jmaranguren89@gmail.com>
2024-03-01 21:39:23 +0100

    Actividad evaluada trimestre 2
    Se ha creado el fetch para coger json, se trata este y se generan opcion para seleccionar categorias

--- Actividad2_JSON/functions.js
+++ Actividad2_JSON/functions.js
@@ -1,26 +1,15 @@
-//Funcion para realizar la carga del json
 export function cargaJson(url) {
-
-
+  return fetch(url) 
+    .then(response => {
+      if (response.status === 200) {
+        console.log("json recibido status 200");
+        return response.json(); 
+      } else {
+        console.log("Estatus no 200, revisar");
+        throw new Error('Respuesta de red OK pero respuesta HTTP no es OK');
+      }
+    })
+    .then(json => {
+      return json; 
+    });
 }
-    
-  // fetch(url).then(response => {
-  //   // esta ok ??
-  //   if (!response.ok) {
-  //     throw new Error('Error obteniendo json');
-  //   }
-  //   // Devuelve los datos
-  //   return response.json();
-  // })
-  // .then(data => {
-  //   // Maneja los datos recibidos
-  //   console.log(data);
-  //   return data
-  // })
-  // .catch(error => {
-  //   console.error('problema en el manejo del json', error);
-  // });}
-
-fetch (url).then(response =>{
-  console.log(response)
-})
