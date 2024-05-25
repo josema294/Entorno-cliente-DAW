db71cf50577ef7b0c99fb1d3c665fa1625c444c1
Jose Maria Aranguren <64466787+josema294@users.noreply.github.com>
2024-05-25 11:19:48 +0200

    terminado ejemplos formularios

--- Simulacro/AngularSImulacro/FormularioAngular/src/app/app.component.ts
+++ Simulacro/AngularSImulacro/FormularioAngular/src/app/app.component.ts
@@ -5,10 +5,17 @@ import { Usuario } from './model/Usuario';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  
 })
 export class AppComponent {
   title = 'FormularioAngular';
   
+  nombre=""
+  edad=""
+  email=""
+  sexo=""
+  comentarios=""
+  opcion=""
   arrayUsuarios: Usuario[] = [];
 
   constructor(){
@@ -40,6 +47,28 @@ export class AppComponent {
 
   }
 
+  //MEtodo con binding de formsmodule
+  agregarUsuarioModule() {
+    const nuevoUsuario = {
+      nombre: this.nombre,
+      edad: this.edad,
+      email: this.email,
+      sexo: this.sexo,
+      comentarios: this.comentarios,
+      opcion: this.opcion
+    };
+    this.arrayUsuarios.push(nuevoUsuario);
+    this.limpiarFormulario();
+  }
+
+  limpiarFormulario() {
+    this.nombre = '';
+    this.edad = "";
+    this.email = '';
+    this.sexo = '';
+    this.comentarios = '';
+    this.opcion = '1';
+  }
 
 
 }
